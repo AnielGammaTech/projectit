6b1e1529d033526233800b6e243ff3165d7345b5
aniel <aniel@gamma.tech>
2025-12-02 22:36:43 +0000

    File changes

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -33,6 +33,7 @@ import { Textarea } from '@/components/ui/textarea';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { cn } from '@/lib/utils';
+import { FolderKanban } from 'lucide-react';
 
 export default function Customers() {
   const [searchQuery, setSearchQuery] = useState('');
