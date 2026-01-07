aeaae0c11d39f01558abd8c8f1ab6a3a0ab037a7
bradd <bradd@gamma.tech>
2026-01-07 17:16:50 +0000

    File changes

--- src/components/modals/TaskDetailModal.jsx
+++ src/components/modals/TaskDetailModal.jsx
@@ -375,14 +375,12 @@ export default function TaskDetailModal({ open, onClose, task, teamMembers = [],
                   )}
                 </button>
               </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
+              <PopoverContent className="w-auto p-0 z-[100]" align="start">
                 <Calendar
                   mode="single"
                   selected={task.due_date ? new Date(task.due_date.split('T')[0] + 'T12:00:00') : undefined}
-                  onSelect={(date) => {
-                    handleDueDateChange(date);
-                  }}
-                  initialFocus
+                  onSelect={handleDueDateChange}
+                  disabled={false}
                 />
                 {task.due_date && (
                   <div className="p-2 border-t">
